d603dfcdcc0325cf844744f203639b0ebb8fbf77
vera <anika_vamp@mail.ru>
2022-11-29 00:00:46 +0300

    task hw2_1

--- 2_lesson/hw2/hw2_1/Program.cs
+++ 2_lesson/hw2/hw2_1/Program.cs
@@ -4,10 +4,25 @@
 // 782 -> 8
 // 918 -> 1
 
+string Cif(int num)
+{
+    if ((-1000 < num && num < -99) || (num < 1000 && num > 99))
+    {
+        return $"{num / 10 % 10}";
+    }
+    else
+    {
+        return ("Число не трехзначное");
+    }
+}
+Console.WriteLine(Cif(int.Parse(Console.ReadLine())));
+
+//
+
 Console.WriteLine("Ведите число: ");
 string a = (Console.ReadLine());
 int a1 = int.Parse(a);
-if (a1 < 999 && a1 > 99)
+if ((-1000 < a1 && a1 < -99) || (a1 < 1000 && a1 > 99))
 {
     int a2 = a1 / 10;
     int a3 = a2 % 10;
@@ -15,24 +30,5 @@ if (a1 < 999 && a1 > 99)
 }
 else
 {
-Console.WriteLine("Число не трехзначное");
-}
-
-
-// или так 
-void Cif(int num)
-{
-    if (num < 999 && num > 99)
-    {
-        int a3 = num / 10;
-        int a2 = a3 % 10;
-        Console.WriteLine(a2);
-    }
-    else
-    {
     Console.WriteLine("Число не трехзначное");
-    }
 }
-Cif(456);
-Cif(782);
-Cif(918);
