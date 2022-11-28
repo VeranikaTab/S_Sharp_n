d10d7e855e337f2b9e08b3ecb08cee48f6b67f10
vera <anika_vamp@mail.ru>
2022-11-29 00:23:52 +0300

    task hw2_2

--- 2_lesson/hw2/hw2_2/Program.cs
+++ 2_lesson/hw2/hw2_2/Program.cs
@@ -1,24 +1,37 @@
-﻿
-// Задача 1: Напишите программу, которая принимает на вход трёхзначное число
-// и на выходе показывает вторую цифру этого числа.
-// 456 -> 5
-// 782 -> 8
-// 918 -> 1
+﻿// Задача 2: Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого числа.
+// 645 -> 65
+// 782 -> 72
+// 918 -> 98
 
-
-void Cif(int num)
+void New(int num)
 {
-    if (num < 999 && num > 99)
-    {
-        int a3 = num / 10;
-        int a2 = a3 % 10;
-        Console.WriteLine(a2);
+    if ((-1000 < num && num < -99) || (num < 1000 && num > 99))
+    { 
+        int j = num / 100;
+        int k = num % 10;
+        int n = j * 10 + k;
+        Console.WriteLine(num);
+        Console.WriteLine(n);
     }
     else
     {
     Console.WriteLine("Число не трехзначное");
     }
 }
-Cif(456);
-Cif(782);
-Cif(918);
+New(new Random().Next(99, 1000));
+
+//
+
+string Cif(int num)
+{
+    if ((-1000 < num && num < -99) || (num < 1000 && num > 99))
+    {
+        return $"{((num / 100)*10) + (num % 10)}";
+    }
+    else
+    {
+        return ("Число не трехзначное");
+    }
+}
+Console.WriteLine(Cif(int.Parse(Console.ReadLine())));
