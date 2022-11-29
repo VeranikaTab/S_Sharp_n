ffb55353fe3b5af485173a6ccb7bec5943691c74
vera <anika_vamp@mail.ru>
2022-11-30 02:43:24 +0300

    task hw2_4

---
+++ 2_lesson/hw2/hw2_4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+void Dni(int cif)
+{
+    if ( (cif >= 6) && (cif < 8))
+    {
+        Console.WriteLine("Да");
+    }
+    else if ((cif <= 5) && (cif > 0))
+    {
+        Console.WriteLine("Нет");
+    }
+    else
+    {
+        Console.WriteLine("Error");
+    }
+}
+Dni(int.Parse(Console.ReadLine()));
