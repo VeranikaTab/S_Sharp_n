070113252a3e4b49e9e652e3106b3d3e1b245fd7
vera <anika_vamp@mail.ru>
2022-12-01 21:10:49 +0300

    task 4_1

---
+++ 4_lesson/4_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 1: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5 
+
+int NumbCount(int n)
+{
+    if(n == 0)
+    {
+        return(1);
+    }
+    int count = 0;
+    while (n > 0)
+    {
+        n = n/10;
+        count++;
+    }
+    return count; 
+    
+}
+Console.WriteLine(NumbCount(int.Parse(Console.ReadLine())));
