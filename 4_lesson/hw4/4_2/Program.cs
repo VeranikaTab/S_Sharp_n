898460adb24ce7d771b337c28d6c22fc3623fcfa
vera <anika_vamp@mail.ru>
2022-12-02 03:15:54 +0300

    task hw4_2

---
+++ 4_lesson/hw4/4_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SumNumbCount(int num)
+{
+    if(num == 0)
+    {
+        return(1);
+    }
+    int count = 0;
+    int sum = 0;
+    while (num > 0)
+    {
+        int ponum = num % 10;
+        sum = sum + ponum;
+        num = num / 10;
+        count++;
+    }
+    return sum; 
+    
+}
+Console.WriteLine(SumNumbCount(int.Parse(Console.ReadLine())));
