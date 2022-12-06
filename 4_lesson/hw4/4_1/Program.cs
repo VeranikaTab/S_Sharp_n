362beb631a8ac4497979cc72baf7c704bf6a5df1
vera <anika_vamp@mail.ru>
2022-12-06 13:46:42 +0300

    task hw4

--- 4_lesson/hw4/4_1/Program.cs
+++ 4_lesson/hw4/4_1/Program.cs
@@ -18,6 +18,28 @@ int Step(int num)
 }
 Console.WriteLine(Step(int.Parse(Console.ReadLine())));
 
+// 2-й вариант
 
+double PowNum(int a, int b)
+{   
+    double n_pow = 1;
+    int b_abs = Math.Abs(b);
+    
+    for (int i = 1; i <= b_abs; i++)
+    {
+        if (b > 0)
+            n_pow *= a; 
+        else
+            n_pow /= a;
+    }  
+    return n_pow;  
+}
+
+Console.WriteLine("Enter a number: ");
+int num_1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter degree of number: ");
+int num_2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(PowNum(num_1, num_2));
 
 
