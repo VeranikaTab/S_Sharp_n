4486614c8a7f3d8f64472eb4f93aaf239e07964b
vera <anika_vamp@mail.ru>
2022-12-20 01:18:41 +0300

    Task hw9

---
+++ 9_lesson/9_0/Program.cs
@@ -0,0 +1,12 @@
+﻿// 1. Задайте значение N. Напишите программу, которая выведет
+//    все натуральные числа в промежутке от 1 до N при помощи рекурсии.
+
+void Chisla(int a)
+{
+    if(a == 0) return;
+    Chisla(a-1);
+    Console.Write($" {a} ");
+}
+Console.WriteLine("Введите значение N: ");
+int a = int.Parse(Console.ReadLine());
+Chisla(a);
---
+++ 9_lesson/9_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// 4. Напишите программу, которая на вход принимает два числа A и B,
+//    и возводит число А в целую степень B с помощью рекурсии.
+
+int Degree (int a, int b)
+{
+    if(b == 0) return 1;
+    return Degree(a, b-1) * a;
+}
+
+
+
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("число А в степени B: ");
+
+Console.WriteLine(Degree(a,b));
